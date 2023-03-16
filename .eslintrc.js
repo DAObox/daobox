fb96996cd86fa0d518c99cab4787c783a76090b1
pythonpete32 <aaron@blackscale.io>
2023-03-16 16:42:32 +0400

    rename daobox

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  // This tells ESLint to load the config from the package `eslint-config-daobox`
+  extends: ["daobox"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@acme/core";
-// import { useIsomorphicLayoutEffect } from "@acme/utils";
+import { Button } from "@daobox/core";
+// import { useIsomorphicLayoutEffect } from "@daobox/utils";
 
 export default function Docs() {
   // useIsomorphicLayoutEffect(() => {
-  //   console.log("Acme docs page");
+  //   console.log("daobox docs page");
   // }, []);
   return (
     <div>
-      <h1>Acme Documentation</h1>
+      <h1>daobox Documentation</h1>
       <Button>Click me</Button>
     </div>
   );
