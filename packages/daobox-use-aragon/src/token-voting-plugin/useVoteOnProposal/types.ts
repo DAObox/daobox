5c6a7be56462e4653d1ff53ea1fb4033995ec443
pythonpete32 <aaron@blackscale.io>
2023-03-18 22:17:47 +0400

    refactor `useUseVoteOnProposal`

--- packages/daobox-use-aragon/src/token-voting-plugin/useVoteOnProposal/types.ts
+++
@@ -1,7 +0,0 @@
-export enum VoteStatus {
-  IDLE = 'idle',
-  WAITING_FOR_SIGNER = 'waitingForSigner',
-  CONFIRMING_TRANSACTION = 'confirmingTransaction',
-  SUCCESS = 'success',
-  ERROR = 'error',
-}
