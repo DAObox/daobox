083b09b899c4c9a82d9e6bf417d256cfcf6404e8
pythonpete32 <aaron@blackscale.io>
2023-03-23 19:00:50 +0400

    add headings

--- apps/example/src/pages/index.tsx
+++ apps/example/src/pages/index.tsx
@@ -3,11 +3,13 @@ import { LinkButton } from "../components/LinkButton";
 function Page() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Core Hooks</h1>
       <LinkButton link="/useFetchDaos" label="useFetchDaos" />
       <LinkButton link="/useFetchDao" label="useFetchDao" />
       <LinkButton link="/useFetchDaoBalances" label="useFetchDaoBalances" />
       <LinkButton link="/useFetchTransfers" label="useFetchTransfers" />
       <LinkButton link="/useEstimateDeposit" label="useEstimateDeposit" />
+      <h1>Token Voting Hooks</h1>
       <LinkButton link="/useFetchProposals" label="useFetchProposals" />
       <LinkButton link="/useCanVote" label="useCanVote" />
       <LinkButton link="/useFetchMembers" label="useFetchMembers" />
