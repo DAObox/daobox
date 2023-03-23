5b64b84ba0c9539d4ec76e31701bf57345242fe0
pythonpete32 <aaron@blackscale.io>
2023-03-23 18:03:17 +0400

    only goerli network

--- apps/example/src/wagmi.ts
+++ apps/example/src/wagmi.ts
@@ -1,9 +1,11 @@
-import { getDefaultClient } from 'connectkit'
-import { createClient } from 'wagmi'
+import { getDefaultClient } from "connectkit";
+import { createClient } from "wagmi";
+import { goerli, mainnet } from "wagmi";
 
 export const client = createClient(
   getDefaultClient({
     autoConnect: true,
-    appName: 'My wagmi + ConnectKit App',
+    appName: "My wagmi + ConnectKit App",
+    chains: [goerli],
   })
-)
+);
