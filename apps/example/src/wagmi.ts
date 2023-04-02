fb865fd90178ad39a82e705a8347484f30da8df9
pythonpete32 <aaron@blackscale.io>
2023-04-02 15:44:41 +0400

    add polygon for testing lens voting

--- apps/example/src/wagmi.ts
+++ apps/example/src/wagmi.ts
@@ -1,11 +1,11 @@
 import { getDefaultClient } from "connectkit";
 import { createClient } from "wagmi";
-import { goerli, mainnet } from "wagmi";
+import { polygon, goerli } from "wagmi/chains";
 
 export const client = createClient(
   getDefaultClient({
     autoConnect: true,
     appName: "My wagmi + ConnectKit App",
-    chains: [goerli],
+    chains: [goerli, polygon],
   })
 );
